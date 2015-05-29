87c1382fbcdf2a8bf0b0e400b914e028d8a897e2
Bob Ashforth <rashforth@marketo.com>
2015-05-29 14:54:44 -0700

    Debugging

--- bin/runrexserver.rb
+++ bin/runrexserver.rb
@@ -4,7 +4,7 @@ require 'rexecute'
 
 # Become deployer. This is necessary because using the Daemons gem to run the rex server as a service
 # makes us the root user, and we need deployer passwordless access.
-Process::UID.grant_privilege(540)
+Process::UID.grant_privilege(502)
 Process::UID.change_privilege(540)
 
 server = RexServer.new()
