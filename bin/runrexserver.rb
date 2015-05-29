04122924a6580667c4e64ae4c0bcc22dbad8d3cc
Bob Ashforth <rashforth@marketo.com>
2015-05-29 08:07:22 -0700

    Adding rexd file and requirement for daemons gem

--- bin/runrexserver.rb
+++ bin/runrexserver.rb
@@ -2,7 +2,8 @@
 
 require 'rexecute'
 
-# Become deployer
+# Become deployer. This is necessary because using the Daemons gem to run the rex server as a service
+# makes us the root user, and we need deployer passwordless access.
 Process::UID.grant_privilege(540)
 Process::UID.change_privilege(540)
 
