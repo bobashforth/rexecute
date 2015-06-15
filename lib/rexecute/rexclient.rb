cd796aa4f71589196bf073203846895b9db33bdc
Bob Ashforth <rashforth@marketo.com>
2015-06-15 09:25:29 -0700

    Debugging

--- lib/rexecute/rexclient.rb
+++ lib/rexecute/rexclient.rb
@@ -198,9 +198,13 @@ class RexClient < RexMessage
           ENV['key'] = value
         end
         exec_command = ""
-        io = IO.popen([cmdenv, "/bin/bash -c /bin/echo #{command}"], mode="r")
-        exec_command = io.read.chomp
-        io.close
+        io = IO.popen([cmdenv, "/bin/bash"], mode="r+") do |io|
+          io.write("echo #{command}")
+          exec_command = io.read.chomp
+          io.close
+        end
+        #exec_command = io.read.chomp
+        #io.close
 
         puts "Executing stepnum #{action.stepnum}: \"#{action.label}\""
         puts "command to be executed is \"#{exec_command}\""
